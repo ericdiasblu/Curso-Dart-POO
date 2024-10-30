9ebda5755376a229c53ac3f49230604bd99d9d7f
ericdiasblu <ericdiasblu@gmail.com>
2024-10-30 17:18:26 -0300

    map folder

---
+++ Maps-Dart---30.10/maps1.dart
@@ -0,0 +1,34 @@
+void main() {
+  // chave-valor
+  Map frutas = Map();
+  frutas["MO"] = "Morango";
+  frutas["TO"] = "Tomate";
+  frutas["BA"] = "Banana";
+
+  print(frutas["TO"]);
+
+  // printa o valor e sua chave
+  Map<int,String> estados = Map();
+  estados[0] = "Santa Catarina";
+  estados[1] = "Parana";
+  estados[2] = "Rio Grande do Sul";
+  estados[3] = "São Paulo";
+  print(estados);
+
+  // printa apenas as keys
+  Map<String,String> estados2 = Map();
+  estados[0] = "Santa Catarina";
+  estados[1] = "Parana";
+  estados[2] = "Rio Grande do Sul";
+  estados[3] = "São Paulo";
+  print(estados.keys);
+
+  // printa os valores do map
+  Map<String,String> estados3 = Map();
+  estados[0] = "Santa Catarina";
+  estados[1] = "Parana";
+  estados[2] = "Rio Grande do Sul";
+  estados[3] = "São Paulo";
+  print(estados.values);
+
+}
